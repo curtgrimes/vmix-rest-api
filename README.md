0c71a15bcd8d4001ecc43d567830dbcff2bfa570
Curt Grimes <gitlab@curtgrimes.com>
2018-02-12 00:16:51 -0600

    Organize routes more, add / route

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 ## Installation
 1. Change the vmix path in app/index.js (TODO: use environment variable)
 1. Build: `docker build . -t vmix-rest-api`
+1. Set environment variable `VMIX_PATH` to something like `http://10.211.55.6:8088/api`
 1. Run locally on port 3000: `docker run -it -p 3000:3000 -d vmix-rest-api`
 1. Go to http://localhost:3000
--- app/index.js
+++ app/index.js
@@ -1,39 +1,18 @@
 var express = require('express');
 var app = express();
-var request = require('request');
-const parseString = require('xml2js').parseString;
+const routes = require('./routes');
 
-const vmixAPIPath = 'http://10.211.55.6:8088/api';
-
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
-
-
-app.get('/inputs', function (req, res) {
-    request(vmixAPIPath, function (error, response, bodyXML) {
-
-        parseString(bodyXML, {attributesKey: 'asdf'}, function (err, result) {
-            res.json(
-                result.vmix.inputs[0].input.map((input) => {
-                    return input['$'];
-                })
-            );
-        });
-    });
+app.use(function (req, res, next) {
+    if (!process.env.VMIX_PATH) {
+        res
+            .status('501')
+            .send('VMIX_PATH environment variable is not set.');
+    }
+    else {
+        next();
+    }
 });
 
-app.get('/inputs/:inputId', function (req, res) {
-    request(vmixAPIPath, function (error, response, bodyXML) {
-
-        parseString(bodyXML, function (err, result) {
-            res.json(
-                result.vmix.inputs[0].input.find((input) => {
-                    return input['$'].key === req.params.inputId;
-                })['$']
-            );
-        });
-    });
-})
+app.use('/', routes);
  
-app.listen(3000)
+app.listen(3000);
