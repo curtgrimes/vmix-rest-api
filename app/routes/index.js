5326c283af2b3f076b751cd7f87f65a087612d4e
Curt Grimes <gitlab@curtgrimes.com>
2018-02-12 22:56:10 -0600

    Add transitions

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,19 +1,26 @@
 const routes = require('express').Router();
 const inputsRoute = require('./inputs');
 const overlaysRoute = require('./overlays');
+const transitionsRoute = require('./transitions');
+
 const vmixService = require('../services/vmix');
-var inputService = require('../services/inputs');
-var overlayService = require('../services/overlays');
+const inputService = require('../services/inputs');
+const overlayService = require('../services/overlays');
+const transitionService = require('../services/transitions');
+
 const lodashGet = require('lodash/get');
 const boolean = require('boolean');
 
+
 routes.use('/inputs', inputsRoute);
 routes.use('/overlays', overlaysRoute);
+routes.use('/transitions', transitionsRoute);
 
 routes.get('/', async (req, res) => {
     let vmixData = await vmixService();
     let inputs = await inputService.all();
     let overlays = await overlayService.all();
+    let transitions = await transitionService.all();
 
     let inputKeys = inputs.map((input) => {
         return input.key;
@@ -23,11 +30,16 @@ routes.get('/', async (req, res) => {
         return overlay.number;
     });
 
+    let transitionsIds = transitions.map((transition) => {
+        return transition.transitionId;
+    });
+
     let response = {
         version: lodashGet(vmixData, 'version[0]._text[0]'),
         edition: lodashGet(vmixData, 'edition[0]._text[0]'),
         inputs: inputKeys,
         overlays: overlayKeys,
+        transitions: transitionsIds,
         preview: lodashGet(vmixData, 'preview[0]._text[0]'),
         active: lodashGet(vmixData, 'active[0]._text[0]'),
         fadeToBlack: boolean(lodashGet(vmixData, 'version[0]._text[0]')),
