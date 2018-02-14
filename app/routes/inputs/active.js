154532958243696a2be4f448de778687a02c8a3f
Curt Grimes <gitlab@curtgrimes.com>
2018-02-13 21:33:32 -0600

    Add /inputs/active and /inputs/preview

---
+++ app/routes/inputs/active.js
@@ -0,0 +1,11 @@
+const getVmixData = require('../../services/vmix');
+const inputService = require('../../services/inputs');
+
+module.exports = async (req, res) => {
+    const all = await inputService.all();
+    const activeInput = all.find((input) => {
+        return input.isActive;
+    });
+
+    return activeInput ? res.json(activeInput) : res.sendStatus(204);
+};
--- app/routes/inputs/index.js
+++ app/routes/inputs/index.js
@@ -1,8 +1,12 @@
 const inputs = require('express').Router();
 const all = require('./all');
 const single = require('./single');
+const active = require('./active');
+const preview = require('./preview');
 
 inputs.get('/', all);
-inputs.get('/:inputId', single);
+inputs.get('/:inputId([0-9]*)', single);
+inputs.get('/active', active);
+inputs.get('/preview', preview);
 
 module.exports = inputs;
