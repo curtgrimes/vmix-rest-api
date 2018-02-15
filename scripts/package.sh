31190a316fcb9e10a588801d1d73eb976802d720
Curt Grimes <gitlab@curtgrimes.com>
2018-02-14 23:54:37 -0600

    Add version number to zip filename

--- scripts/package.sh
+++ scripts/package.sh
@@ -6,5 +6,5 @@ rm -f vmix-rest-api.zip # if it already exists
 pkg --targets latest-win-x64 --output .tmp/$(echo $EXE_FILENAME) app/index.js
 cp config/config.sample.yml .tmp
 mv .tmp/config.sample.yml .tmp/config.yml
-zip -j vmix-rest-api.zip .tmp/$(echo $EXE_FILENAME) .tmp/config.yml
+zip -j vmix-rest-api-$(echo $VERSION)-windows.zip .tmp/$(echo $EXE_FILENAME) .tmp/config.yml
 rm -rf .tmp
