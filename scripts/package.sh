a4c4abba45c3836a36d76722ae3356b75c4f2672
Curt Grimes <gitlab@curtgrimes.com>
2018-02-14 23:52:39 -0600

    Delete the zip if it already exists when starting packaging

--- scripts/package.sh
+++ scripts/package.sh
@@ -2,6 +2,7 @@
 VERSION=`npm run -s version`
 EXE_FILENAME="vmix-rest-api-$(echo $VERSION).exe"
 mkdir .tmp
+rm -f vmix-rest-api.zip # if it already exists
 pkg --targets latest-win-x64 --output .tmp/$(echo $EXE_FILENAME) app/index.js
 cp config/config.sample.yml .tmp
 mv .tmp/config.sample.yml .tmp/config.yml
